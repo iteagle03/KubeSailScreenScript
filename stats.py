20f42e8dafa293d172479e8561b4c04fd889e43e
Sean Hiatt <email@seanhiatt.ws>
2023-02-05 22:12:47 -0600

    Initial commit

---
+++ stats.py
@@ -0,0 +1,136 @@
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+# -*- coding: utf-8 -*-
+
+import time
+import subprocess
+import digitalio
+import board
+import psutil
+from PIL import Image, ImageDraw, ImageFont
+from adafruit_rgb_display import st7789
+
+
+# Configuration for CS and DC pins (these are FeatherWing defaults on M0/M4):
+cs_pin = digitalio.DigitalInOut(board.CE0)
+dc_pin = digitalio.DigitalInOut(board.D25)
+reset_pin = None
+
+# Config for display baudrate (default max is 24mhz):
+BAUDRATE = 64000000
+
+# Setup SPI bus using hardware SPI:
+spi = board.SPI()
+
+# Create the ST7789 display:
+disp = st7789.ST7789(
+    spi,
+    cs=cs_pin,
+    dc=dc_pin,
+    rst=reset_pin,
+    baudrate=BAUDRATE,
+    width=240,
+    height=240,
+    x_offset=0,
+    y_offset=80,
+)
+
+# Create blank image for drawing.
+# Make sure to create image with mode 'RGB' for full color.
+height = disp.width  # we swap height/width to rotate it to landscape!
+width = disp.height
+image = Image.new("RGB", (width, height))
+rotation = 180
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Draw a black filled box to clear the image.
+draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
+disp.image(image, rotation)
+# Draw some shapes.
+# First define some constants to allow easy resizing of shapes.
+padding = -2
+top = padding
+bottom = height - padding
+# Move left to right keeping track of the current x position for drawing shapes.
+x = 0
+
+
+# Alternatively load a TTF font.  Make sure the .ttf font file is in the
+# same directory as the python script!
+# Some other nice fonts to try: http://www.dafont.com/bitmap.php
+font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 24)
+
+# Turn on the backlight
+backlight = digitalio.DigitalInOut(board.D22)
+backlight.switch_to_output()
+backlight.value = True
+
+time.sleep(5)
+
+# Used for Calculating Bytes
+def get_size(bytes):
+    """
+    Returns size of bytes in a nice format
+    """
+    for unit in ['', 'K', 'M', 'G', 'T', 'P']:
+        if bytes < 1024:
+            return f"{bytes:.2f}{unit}B"
+        bytes /= 1024
+
+# get the network I/O stats from psutil
+io = psutil.net_io_counters()
+UPDATE_DELAY = 1
+# extract the total bytes sent and received
+bytes_sent, bytes_recv = io.bytes_sent, io.bytes_recv
+
+while True:
+    time.sleep(UPDATE_DELAY)
+    # Draw a black filled box to clear the image.
+    draw.rectangle((0, 0, width, height), outline=0, fill=0)
+
+    # Shell scripts for system monitoring from here:
+    # https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
+    cmd = "hostname -I | cut -d' ' -f1"
+    IP = "IP: " + subprocess.check_output(cmd, shell=True).decode("utf-8")
+    cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
+    CPU = subprocess.check_output(cmd, shell=True).decode("utf-8")
+    cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%s MB  %.2f%%\", $3,$2,$3*100/$2 }'"
+    MemUsage = subprocess.check_output(cmd, shell=True).decode("utf-8")
+    cmd = 'df -h | awk \'$NF=="/"{printf "Disk: %d/%d GB  %s", $3,$2,$5}\''
+    Disk = subprocess.check_output(cmd, shell=True).decode("utf-8")
+    cmd = "cat /sys/class/thermal/thermal_zone0/temp |  awk '{printf \"CPU Temp: %.1f F\", $(NF-0) / 1000 * 9 / 5 + 32}'"  # pylint: disable=line-too-long
+    Temp = subprocess.check_output(cmd, shell=True).decode("utf-8")
+    io_2 = psutil.net_io_counters()
+    us, ds = io_2.bytes_sent - bytes_sent, io_2.bytes_recv - bytes_recv
+    Upload =   str('Tx: ' + (get_size((us)/UPDATE_DELAY)) + '/s')
+    Download = str('Rx: ' + (get_size((ds)/UPDATE_DELAY)) + '/s')
+    Date = str(time.strftime("%m-%d-%Y"))
+    Time = str(time.strftime("%I:%M:%S %p"))
+
+    # Write lines of text.
+    y = top
+    draw.text((x, y), IP, font=font, fill="#FFFFFF")
+    y += font.getsize(IP)[1]
+    draw.text((x, y), CPU, font=font, fill="#FFFF00")
+    y += font.getsize(CPU)[1]
+    draw.text((x, y), MemUsage, font=font, fill="#00FF00")
+    y += font.getsize(MemUsage)[1]
+    draw.text((x, y), Disk, font=font, fill="#0000FF")
+    y += font.getsize(Disk)[1]
+    draw.text((x, y), Temp, font=font, fill="#FF00FF")
+    y += font.getsize(str(Upload))[1]
+    draw.text((x, y), Upload, font=font, fill="#FF0000")
+    y += font.getsize(str(Download))[1]
+    draw.text((x, y), Download, font=font, fill="#00FF00")
+    y += font.getsize(str(Date))[1]
+    draw.text((x, y), Date, font=font, fill="#FFFFFF")
+    y += font.getsize(str(Time))[1]
+    draw.text((x, y), Time, font=font, fill="#FFFFFF")
+
+    # Display image.
+    disp.image(image, rotation)
+    # time.sleep(0.1)
+    bytes_sent, bytes_recv = io_2.bytes_sent, io_2.bytes_recv
